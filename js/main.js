2fd666d72f220e5d8a6507d386ffa6b793b93f55
joolivero192002-art <joolivero192002@gmail.com>
2026-09-05 18:41:31 -0500

    feat: diseño responsive del header, marcas, footer completados

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+console.log(menuToggle);
+console.log(nav);menuToggle.addEventListener("click", function() {
+    nav.classList.toggle("nav-active");
+});
