c1e8c4780740809fe0178062e60efe076d29ed6d
joolivero192002-art <joolivero192002@gmail.com>
2026-09-07 19:18:34 -0500

    agregar funcionalidad al diseño responsive del nav

--- js/main.js
+++ js/main.js
@@ -4,4 +4,4 @@ const nav = document.querySelector("nav");
 console.log(menuToggle);
 console.log(nav);menuToggle.addEventListener("click", function() {
     nav.classList.toggle("nav-active");
-});
+}); 
