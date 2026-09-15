abd2bfa7f28472d12f01aeef32651d8064272485
joolivero192002-art <joolivero192002@gmail.com>
2026-09-15 17:06:39 -0500

    feat: añadir renderizado dinámico de productos y eliminar tarjetas de producto codificadas de forma fija

--- js/main.js
+++ js/main.js
@@ -14,3 +14,54 @@ navLinks.forEach(function (link) {
         nav.classList.remove("nav-active");
     });
 })
+
+const productos = [
+    {
+        nombre: "Rolex submariner",
+        precio:  0,
+        imagen:  "img/Productos-Destacados/Rolex_Submariner.png"
+    },
+    {
+        nombre: "Hublot Big Bang Unico",
+        precio: 0,
+        imagen: "img/Productos-Destacados/Hublot_BigBangUnico.png"
+    },
+    {
+        nombre: "Richard Mille RM RM011",
+        precio: 0,
+        imagen: "img/Productos-Destacados/RichardMille_RM011.png"
+    },
+    {
+        nombre: "Patek Philippe Nautilus",
+        precio: 0,
+        imagen: "img/Productos-Destacados/PatekPhilippe_Nautilus.webp"
+    },
+    {
+        nombre: "Cartier Santos",
+        precio: 0,
+        imagen: "img/Productos-Destacados/Cartier_Santos.png"
+    },
+    {
+        nombre: "Curren 8329",
+        precio: 0,
+        imagen: "img/Productos-Destacados/Curren_8329.webp"
+    }
+]; console.log(productos); 
+productos.forEach(function(producto) {
+    console.log(producto.nombre);
+});
+const productsGrid = document.querySelector(".products-grid");
+
+productos.forEach(function(producto) {
+    const tarjeta = document.createElement("article");
+    tarjeta.classList.add("product-card");
+
+    tarjeta.innerHTML = `
+        <img src="${producto.imagen}" alt="${producto.nombre}" loading="lazy">
+        <h3>${producto.nombre}</h3>
+        <p class="price">$${producto.precio},00</p>
+        <a href="#" class="btn-primary">Agregar al carrito</a>
+    `;
+
+    productsGrid.appendChild(tarjeta);
+});
